f999ad0bf6d896d382137ea61d17ec2ce911ed4a
lencx <cxin1314@gmail.com>
2020-02-14 23:40:44 +0800

    chore: share

--- src/pages/recharge/components/navbar.jsx
+++ src/pages/recharge/components/navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import router from 'umi/router';
+import copy from 'copy-to-clipboard';
 import { checkLogin } from '@/utils/tools';
 import styles from './index.less';
 
@@ -12,7 +13,10 @@ export default function RechargeNavbar() {
     });
   };
   const handleShare = () => {
-    alert('Share');
+    // alert('Share');
+    // TODO: 接口返回分享链接
+    // 分享到微信，微博，QQ等
+    copy('这里给游戏充值最低3折，同样消费加倍快乐，打折传送门：http://xxx.com');
   };
   return (
     <div className={styles.navbar}>
