25e1fbe5ec4d007f3e094d1a52bae7dfa5559f56
lencx <cxin1314@gmail.com>
2020-02-17 15:55:15 +0800

    build

--- dist/sw.js
+++ dist/sw.js
@@ -15,7 +15,7 @@ importScripts("/workbox-v3.6.3/workbox-sw.js");
 workbox.setConfig({modulePathPrefix: "/workbox-v3.6.3"});
 
 importScripts(
-  "/precache-manifest.e60ed02d0471e568f29e2e63b3ae4619.js"
+  "/precache-manifest.47ce2054110c0fb528fa476646562065.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "ants"});
