b0d35c28d3f61409ad5ffd6628420389c97d68fe
lencx <cxin1314@gmail.com>
2020-02-17 14:03:03 +0800

    chore: api

--- start.sh
+++ start.sh
@@ -2,24 +2,24 @@
 if [ $DEPLOY_ENV = 'dev' ]
 then
   sed -i 's,<dist>,/root/app/dist,' /etc/nginx/conf.d/nginx_app.conf
-  sed -i 's,<capi_url>,https://yxbservice-test.zhongan.io,' /etc/nginx/conf.d/nginx_app.conf
+  sed -i 's,<capi_url>,https://ants-backend-test.zhongan.io,' /etc/nginx/conf.d/nginx_app.conf
 elif [ $DEPLOY_ENV = 'test' ]
 then
   sed -i 's,<dist>,/root/app/dist,' /etc/nginx/conf.d/nginx_app.conf
   if [ $PROJECT_ID -eq '8805' ]
   then
-    sed -i 's,<capi_url>,https://yxbservice-test.zhongan.io,' /etc/nginx/conf.d/nginx_app.conf
+    sed -i 's,<capi_url>,https://ants-backend-test.zhongan.io,' /etc/nginx/conf.d/nginx_app.conf
   else
-    sed -i 's,<capi_url>,https://yxbservice-test.zhongan.io,' /etc/nginx/conf.d/nginx_app.conf
+    sed -i 's,<capi_url>,https://ants-backend-test.zhongan.io,' /etc/nginx/conf.d/nginx_app.conf
   fi
 elif [ $DEPLOY_ENV = 'pre' ]
 then
   sed -i 's,<dist>,/root/app/dist,' /etc/nginx/conf.d/nginx_app.conf
-  sed -i 's,<capi_url>,https://yxbservice-test.zhongan.io,' /etc/nginx/conf.d/nginx_app.conf
+  sed -i 's,<capi_url>,https://ants-backend-test.zhongan.io,' /etc/nginx/conf.d/nginx_app.conf
 elif [ $DEPLOY_ENV = 'prd' ]
 then
   sed -i 's,<dist>,/root/app/app,' /etc/nginx/conf.d/nginx_app.conf
-  sed -i 's,<capi_url>,https://yxbservice-test.zhongan.io,' /etc/nginx/conf.d/nginx_app.conf
+  sed -i 's,<capi_url>,https://ants-backend-test.zhongan.io,' /etc/nginx/conf.d/nginx_app.conf
 fi
 
 mv nginx.default.conf /etc/nginx/conf.d/default.conf
